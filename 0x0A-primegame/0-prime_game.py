5540b9088f4508e6a9b95e4a8aee6e720d3a60e0
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-12-12 19:26:13 +0000

    Fix task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
 def isWinner(x, nums):
+    """
+    Determine the winner of the prime number game.
+
+    Args:
+        x (int): Number of rounds.
+        nums (list): List of n values for each round.
+
+    Returns:
+        str or None: Name of the winner ("Maria" or "Ben"),
+                    or None if there's a tie.
+    """
     def sieve_of_eratosthenes(max_n):
         """Generate a list of prime counts up to max_n."""
         sieve = [True] * (max_n + 1)
@@ -23,19 +34,20 @@ def isWinner(x, nums):
 
     max_n = max(nums)
     prime_counts = sieve_of_eratosthenes(max_n)
-    
+
     maria_wins = 0
     ben_wins = 0
-    
+
     for n in nums:
         if prime_counts[n] % 2 == 1:
             maria_wins += 1
         else:
             ben_wins += 1
-    
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
+

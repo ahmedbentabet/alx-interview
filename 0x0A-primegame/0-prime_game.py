dfd35c813f99088bf180e41b1cdd49de35b888df
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-12-12 19:28:57 +0000

    Fix task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,52 +1,93 @@
 #!/usr/bin/python3
+"""
+Module to solve the Prime Game problem.
+
+This module determines the winner of a game where players take turns
+removing prime numbers and their multiples from a set of consecutive integers.
+"""
+
 
 def isWinner(x, nums):
     """
-    Determine the winner of the prime number game.
+    Determine the winner of the prime game.
 
     Args:
-        x (int): Number of rounds.
-        nums (list): List of n values for each round.
+        x (int): Number of game rounds
+        nums (list): List of n values for each round
 
     Returns:
-        str or None: Name of the winner ("Maria" or "Ben"),
-                    or None if there's a tie.
+        str or None: Name of the player who won the most rounds
     """
-    def sieve_of_eratosthenes(max_n):
-        """Generate a list of prime counts up to max_n."""
-        sieve = [True] * (max_n + 1)
-        sieve[0] = sieve[1] = False  # 0 and 1 are not prime
-        for i in range(2, int(max_n**0.5) + 1):
-            if sieve[i]:
-                for j in range(i * i, max_n + 1, i):
-                    sieve[j] = False
-        # Count primes up to each number
-        prime_counts = [0] * (max_n + 1)
-        count = 0
-        for i in range(1, max_n + 1):
-            if sieve[i]:
-                count += 1
-            prime_counts[i] = count
-        return prime_counts
+    def is_prime(n):
+        """
+        Check if a number is prime.
 
-    if x < 1 or not nums:
-        return None
+        Args:
+            n (int): Number to check
 
-    max_n = max(nums)
-    prime_counts = sieve_of_eratosthenes(max_n)
+        Returns:
+            bool: True if prime, False otherwise
+        """
+        if n < 2:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
 
+    def play_game(n):
+        """
+        Simulate a single round of the prime game.
+
+        Args:
+            n (int): Upper limit of the consecutive integers set
+
+        Returns:
+            str: Name of the winner ('Maria' or 'Ben')
+        """
+        # If n is too small, Ben wins immediately
+        if n < 2:
+            return 'Ben'
+
+        # Track available numbers
+        available = [True] * (n + 1)
+        available[0] = available[1] = False
+
+        current_player = 'Maria'
+        while True:
+            # Find the next prime number
+            prime_found = False
+            for prime in range(2, n + 1):
+                if available[prime] and is_prime(prime):
+                    prime_found = True
+                    # Remove prime and its multiples
+                    available[prime] = False
+                    for multiple in range(prime, n + 1, prime):
+                        available[multiple] = False
+                    break
+
+            # If no prime found, current player loses
+            if not prime_found:
+                return 'Ben' if current_player == 'Maria' else 'Maria'
+
+            # Switch players
+            current_player = 'Ben' if current_player == 'Maria' else 'Maria'
+
+    # Track wins for Maria and Ben
     maria_wins = 0
     ben_wins = 0
 
+    # Play each round
     for n in nums:
-        if prime_counts[n] % 2 == 1:
+        winner = play_game(n)
+        if winner == 'Maria':
             maria_wins += 1
         else:
             ben_wins += 1
 
+    # Determine overall winner
     if maria_wins > ben_wins:
-        return "Maria"
+        return 'Maria'
     elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+        return 'Ben'
+    return None
